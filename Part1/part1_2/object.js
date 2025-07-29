21263025222ad3a3589df8476159d938850e6ce7
rmpurohit <purohitmahendrarishi@gmail.com>
2025-07-29 17:59:52 +0200

    objects in JS

---
+++ Part1/part1_2/object.js
@@ -0,0 +1,24 @@
+const object1 = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+  }
+  
+  const object2 = {
+    name: 'Full Stack web application development',
+    level: 'intermediate studies',
+    size: 5,
+  }
+  
+  const object3 = {
+    name: {
+      first: 'Dan',
+      last: 'Abramov',
+    },
+    grades: [2, 3, 5, 3],
+    department: 'Stanford University',
+  }
+
+  console.log("obj1 =======> ",object1)
+  console.log("obj2 =======> ",object2)
+  console.log("obj3 =======> ",object3)
