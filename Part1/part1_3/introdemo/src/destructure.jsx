722593d76ba5c9b412856a4d0b5ff7a1ec1faac5
rmpurohit <purohitmahendrarishi@gmail.com>
2025-07-29 20:41:48 +0200

    destructuring react component helper function eg birthyear

---
+++ Part1/part1_3/introdemo/src/destructure.jsx
@@ -0,0 +1,25 @@
+const Hello = (props) => {
+    const name = props.name
+    const age = props.age
+  
+    const bornYear = () => new Date().getFullYear() - age
+  
+    return (
+      <div>
+        <p>Hello {name}, you are {age} years old</p>
+        <p>So you were probably born in {bornYear()}</p>
+      </div>
+    )
+  }
+
+  const Hello = (props) => {
+    const { name, age } = props
+    const bornYear = () => new Date().getFullYear() - age
+  
+    return (
+      <div>
+        <p>Hello {name}, you are {age} years old</p>
+        <p>So you were probably born in {bornYear()}</p>
+      </div>
+    )
+  }
