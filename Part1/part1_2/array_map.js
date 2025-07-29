85b8847bbd7ff6403e5917e4cc1bd8e0f0a6db16
rmpurohit <purohitmahendrarishi@gmail.com>
2025-07-29 17:52:47 +0200

    map js array to html li

--- Part1/part1_2/array_map.js
+++ Part1/part1_2/array_map.js
@@ -2,3 +2,7 @@ const t = [1, 2, 3]
 
 const m1 = t.map(value => value * 2)
 console.log(m1)   // [2, 4, 6] is printed
+
+const m2 = t.map(value => '<li>' + value + '</li>')
+console.log(m2)  
+// [ '<li>1</li>', '<li>2</li>', '<li>3</li>' ] is printed
