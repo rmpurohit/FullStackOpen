07e48285a54b0b9f756ceaafe4bccb27894fd268
rmpurohit <purohitmahendrarishi@gmail.com>
2025-07-29 20:44:46 +0200

    Hello with destructuring props

--- Part1/part1_3/introdemo/src/destructure.jsx
+++ Part1/part1_3/introdemo/src/destructure.jsx
@@ -23,3 +23,20 @@ const Hello = (props) => {
       </div>
     )
   }
+
+  const HelloT = (props) => {
+    const { name, age } = props
+  }
+
+  const HelloDestructure = ({ name, age }) => {
+    const bornYear = () => new Date().getFullYear() - age
+  
+    return (
+      <div>
+        <p>
+          Hello {name}, you are {age} years old
+        </p>
+        <p>So you were probably born in {bornYear()}</p>
+      </div>
+    )
+  }
