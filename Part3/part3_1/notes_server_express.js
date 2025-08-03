164947278545f458e4a7f7153daf3fd5f2df493c
rmpurohit <purohitmahendrarishi@gmail.com>
2025-08-03 14:13:30 +0200

    phonebook using express

--- Part3/part3_1/notes_server_express.js
+++ Part3/part3_1/notes_server_express.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
 let notes = [
     {
@@ -26,6 +27,52 @@ app.get('/', (request, response) => {
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    const note = notes.find(note => note.id === id)
+    if (note) {
+        response.json(note)
+    } else {
+        //response.status(404).json({ message: "Note not found" })
+        response.status(404).send('Note not found')
+        response.status(404).end()
+    }
+  })
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = request.params.id
+    notes = notes.filter(note => note.id !== id)
+  
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+      ? Math.max(...notes.map(n => Number(n.id)))
+      : 0
+    return String(maxId + 1)
+  }
+  
+  app.post('/api/notes', (request, response) => {
+    const body = request.body
+  
+    if (!body.content) {
+      return response.status(400).json({ 
+        error: 'content missing' 
+      })
+    }
+  
+    const note = {
+      id: generateId(),
+      content: body.content,
+      important: body.important || false,
+    }
+  
+    notes = notes.concat(note)
+  
+    response.json(note)
+  })
 
 const PORT = 3001
 app.listen(PORT, () => {
